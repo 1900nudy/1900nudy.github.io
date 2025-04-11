1b1d61cd08c3689ab34aa5bc7eed603304c473e0
Hunter Phillips <hsphillips@my.waketech.edu>
2025-04-11 16:57:44 -0400

    Create gradebook.js

---
+++ gradebook.js
@@ -0,0 +1,9 @@
+function fetchGradeData() {
+    console.log("Fetching grade data...");
+}
+function populatedGradebook(data) {
+    console.log("Populating gradebook with data:", data);
+}
+
+const gradeData = fetchGradeData();
+populateGradebook(gradeData);
