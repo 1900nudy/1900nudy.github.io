ac57c0b338d0b131d70351ae5cd283689d4d4eec
Hunter Phillips <hsphillips@my.waketech.edu>
2025-04-30 14:00:19 -0400

    Update gradebook.js

--- gradebook.js
+++ gradebook.js
@@ -7,3 +7,24 @@ function populatedGradebook(data) {
 
 const gradeData = fetchGradeData();
 populateGradebook(gradeData);
+function fetchGradeData() { 
+    
+console.log("Fetching grade data...");
+
+let xhr = new XMLHttRequest();
+
+let apiRoute = "/api/grades";
+
+xhr.onreadystatechange = function(){
+
+    if(xhr.readyState === xhr.DONE){
+        if (xhr.status !==200){
+            console.error('Could not get grades.   
+                          Status: ${xhr.status}');
+
+        populateGradebook(JSON.parse(xhr.responseText));
+        }
+    }.bind(this); 
+    xhr.open("get", apiroute, true);
+    xhr.send();
+}
